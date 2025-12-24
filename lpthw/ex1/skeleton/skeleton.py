6e084e19a148e9c54c1b377261991a36bf20266a
sanmaomao-000 <neimengdemajie@gmail.com>
2025-12-24 19:07:04 +0800

    Add files via upload

---
+++ lpthw/ex1/skeleton/skeleton.py
@@ -0,0 +1,6 @@
+ 
+def answer():
+    return 42
+def add(a,b):
+    result = a + b
+    return result
---
+++ lpthw/ex1/tests/test_skeleton.py
@@ -0,0 +1,6 @@
+ 
+from skeleton import skeleton
+def test_basic():
+    assert skeleton.answer() == 42
+def test_add_result():
+    assert skeleton.add(2,3) == 4 
